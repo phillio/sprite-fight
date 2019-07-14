79c9a779e49c0b8714b6c069b02100d26ca6010b
Phil Ma <phillio411@gmail.com>
2019-07-14 17:15:43 -0400

    add player 2 functionality

--- design.js
+++ design.js
@@ -25,19 +25,22 @@
 //https://stackoverflow.com/questions/48545764/moving-div-using-javascript
 
 document.onkeydown = function(e) {
-    const UpBtn = 87;
-    const DownBtn = 83;
-    const space = 32;
+    const p1Up = 87;
+    const p1Down = 83;
+    const p1Fire = 67;
+    const p2Up = 80;
+    const p2Down = 76;
+    const p2Fire = 77;
     const gotu = document.querySelector(".gotu-standing")
     const frizu = document.querySelector(".frizu-standing")
   
-    if (e.keyCode === DownBtn) {
+    if (e.keyCode === p1Down) {
         e.preventDefault();
         if (parseInt(gotu.style.top) <= 210) {
             gotuDown = parseInt(gotu.style.top);
             gotu.style.top = (gotuDown + 15) + 'px';
         }
-    } else if (e.keyCode === UpBtn) {
+    } else if (e.keyCode === p1Up) {
         e.preventDefault();
         gotuUp = parseInt(gotu.style.top || 227);
         gotu.style.top = (gotuUp - 15) + 'px';
@@ -45,7 +48,7 @@ document.onkeydown = function(e) {
             gotu.style.top = (gotuUp) + 'px';
             console.log('STOP')
         }
-    } else if (e.keyCode === space) {
+    } else if (e.keyCode === p1Fire) {
         const kamehameha = document.createElement('div')
         kamehameha.className = "kamehameha"
         document.body.appendChild(kamehameha);
@@ -60,23 +63,73 @@ document.onkeydown = function(e) {
             const kamehamehaDiv = kamehameha.getBoundingClientRect();
             const frizu = document.querySelector(".frizu-standing");
             const frizuDiv = frizu.getBoundingClientRect();
-            if (kamehamehaDiv.bottom > frizuDiv.top && kamehamehaDiv.top < frizuDiv.top) {
+            const kameAdjTop = ((kamehamehaDiv.top-22))/1.5;
+            const kameAdjBot = ((kamehamehaDiv.bottom-22))/1.5;
+            if (kameAdjBot > frizuDiv.top && kameAdjTop < frizuDiv.top) {
                 console.log('HIT - DEDUCT HP')
                 //https://stackoverflow.com/questions/20277052/how-to-make-a-health-bar
-                let healthPoints = document.querySelector("#health")
+                let healthPoints = document.querySelector("#p2health")
                 healthPoints.value -= 5;
             } else if (frizuDiv.bottom > kamehamehaDiv.top && frizuDiv.top < kamehamehaDiv.top) {
                 console.log('HIT - DEDUCT HP')
-                let healthPoints = document.querySelector("#health")
+                let healthPoints = document.querySelector("#p2health")
                 healthPoints.value -= 5;
             } else {
                 console.log('MISS - LOL GET BETTER')
             }
                 
-        }, 500)
+        }, 500);
         setTimeout(function() {
             kamehameha.parentNode.removeChild(kamehameha)
-        }, 501)
+        }, 501);
+    }  else if (e.keyCode === p2Down) {
+        e.preventDefault();
+        if (parseInt(frizu.style.top) <= 210) {
+            frizuDown = parseInt(frizu.style.top);
+            frizu.style.top = (frizuDown + 15) + 'px';
+        }
+    } else if (e.keyCode === p2Up) {
+        e.preventDefault();
+        frizuUp = parseInt(frizu.style.top || 227);
+        frizu.style.top = (frizuUp - 15) + 'px';
+          if (parseInt(frizu.style.top) <= 10) {
+            frizu.style.top = (frizuUp) + 'px';
+            console.log('STOP')
+        }
+    } else if (e.keyCode === p2Fire) {
+        const kamehameha2 = document.createElement('div')
+        kamehameha2.className = "kamehameha2"
+        document.body.appendChild(kamehameha2);
+        kamehamehaGPST2 = parseInt(frizu.style.top || 227);
+        kamehameha2.style.top = ((kamehamehaGPST2*1.5)+22) + 'px';
+        kamehameha2.style.left = 128 + 'px';
+        setTimeout(function() {
+            const frizu = document.querySelector('.frizu-standing');
+            //emre mentinoed getBoundingClientRect()
+            const frizuDiv = frizu.getBoundingClientRect();
+            const kamehameha2 = document.querySelector('.kamehameha2');
+            const kamehameha2Div = kamehameha2.getBoundingClientRect();
+            const gotu = document.querySelector(".gotu-standing");
+            const gotuDiv = gotu.getBoundingClientRect();
+            const kameAdjTop2 = ((kamehameha2Div.top)-22)/1.5;
+            const kameAdjBot2 = ((kamehameha2Div.bottom)-22)/1.5;
+            if (kameAdjBot2 > gotuDiv.top && kameAdjTop2 < gotuDiv.top) {
+                console.log('HIT - DEDUCT HP')
+                //https://stackoverflow.com/questions/20277052/how-to-make-a-health-bar
+                let healthPoints2 = document.querySelector("#p1health")
+                healthPoints2.value -= 5;
+            } else if (gotuDiv.bottom > kamehameha2Div.top && gotuDiv.top < kamehameha2Div.top) {
+                console.log('HIT - DEDUCT HP')
+                let healthPoints2 = document.querySelector("#p1health")
+                healthPoints2.value -= 5;
+            } else {
+                console.log('MISS - LOL GET BETTER')
+            }
+                
+        }, 500);
+        setTimeout(function() {
+            kamehameha2.parentNode.removeChild(kamehameha2)
+        }, 501);
     }
   }
   
@@ -216,11 +269,11 @@ document.onkeydown = function(e) {
 //attempt 4
 
 // document.onkeydown = function(e) {
-//   const UpBtn = 9;
-//   const DownBtn = 16;
+//   const p1Up = 9;
+//   const p1Down = 16;
 //   const gotu = document.querySelector(".gotu-standing")
 
-//   if (e.keyCode === DownBtn) {
+//   if (e.keyCode === p1Down) {
 //         e.preventDefault();
 //         gotuGPS = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS + 15) + 'px';
@@ -228,7 +281,7 @@ document.onkeydown = function(e) {
 //         const gotuDown = document.querySelector(".gotu-down")
 //         return gotuDown
 //   }
-//   if (e.keyCode === UpBtn) {
+//   if (e.keyCode === p1Up) {
 //         e.preventDefault();
 //         gotuGPS = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS - 15) + 'px';
@@ -237,17 +290,17 @@ document.onkeydown = function(e) {
 // }
 
 // document.onkeyup = function(e) {
-//     const UpBtn = 9;
-//     const DownBtn = 16;
+//     const p1Up = 9;
+//     const p1Down = 16;
 //     const gotu = document.querySelector(".gotu-standing")
     
-//     if (e.keyCode === DownBtn) {
+//     if (e.keyCode === p1Down) {
         
 //         const gotuDown = document.querySelector(".gotu-down")
 //         gotuDown.className = "gotu-standing";
 
 //     }
-//     if (e.keyCode === UpBtn) {
+//     if (e.keyCode === p1Up) {
 
 //         const gotuUp = document.querySelector(".gotu-up")
 //         gotuUp.className = "gotu-standing";
@@ -259,20 +312,20 @@ document.onkeydown = function(e) {
 //attempt 5
 
 // document.onkeydown = function(e) {
-//   const UpBtn = 9;
-//   const DownBtn = 16;
+//   const p1Up = 9;
+//   const p1Down = 16;
 //   const gotu = document.querySelector(".gotu-standing")
 //   const gotuGPS = document.querySelector(".gotu-down")
 //   const gotuUp = document.querySelector("gotu-up")
 
-//   if (e.keyCode === DownBtn) {
+//   if (e.keyCode === p1Down) {
 //         e.preventDefault();
 //         gotuGPS = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS + 15) + 'px';
 //         gotu.classList.toggle("gotu-down");
 //         gotu.classList.toggle("gotu-standing");
 //   }
-//   if (e.keyCode === UpBtn) {
+//   if (e.keyCode === p1Up) {
 //         e.preventDefault();
 //         gotuUp = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS - 15) + 'px';
@@ -282,18 +335,18 @@ document.onkeydown = function(e) {
 // }
 
 // document.onkeyup = function(e) {
-//     const UpBtn = 9;
-//     const DownBtn = 16;
+//     const p1Up = 9;
+//     const p1Down = 16;
 //     const gotuUp = document.querySelector(".gotu-up")
 //     const gotuGPS = document.querySelector(".gotu-down")
 //     const gotu = document.querySelector(".gotu-standing")
   
-//     if (e.keyCode === DownBtn) {
+//     if (e.keyCode === p1Down) {
 //           e.preventDefault();
 //           gotuGPS.classList.toggle("gotu-standing");
 //           return gotu
 //     }
-//     if (e.keyCode === UpBtn) {
+//     if (e.keyCode === p1Up) {
 //           e.preventDefault();
 //           gotuUp.classList.toggle("gotu-standing");
 //           return gotu
@@ -305,11 +358,11 @@ document.onkeydown = function(e) {
 
 
 // document.onkeydown = function(e) {
-//   const UpBtn = 9;
-//   const DownBtn = 16;
+//   const p1Up = 9;
+//   const p1Down = 16;
 //   const gotu = document.querySelector(".gotu-standing")
 
-//   if (e.keyCode === DownBtn) {
+//   if (e.keyCode === p1Down) {
 //         e.preventDefault();
 //         gotuGPS = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS + 15) + 'px';
@@ -318,7 +371,7 @@ document.onkeydown = function(e) {
 //         gotuGPS.classList.toggle("gotu-standing");
 //         return gotu;
 //   }
-//   if (e.keyCode === UpBtn) {
+//   if (e.keyCode === p1Up) {
 //         e.preventDefault();
 //         gotuGPS = parseInt(gotu.style.top || 0);
 //         gotu.style.top = (gotuGPS - 15) + 'px';
