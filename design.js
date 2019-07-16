cd88c11eebb754c883ca384410c3bab0eb65acfb
Phil Ma <phillio411@gmail.com>
2019-07-16 01:05:42 -0400

    add aura css

--- design.js
+++ design.js
@@ -1,6 +1,53 @@
 /* eslint-disable */
 
-//https://stackoverflow.com/questions/48545764/moving-div-using-javascript
+setTimeout(() => {
+    makeBoard();
+}, 100);
+
+const makeBoard = function() {
+    makeSprites();
+    makeHealthBars();
+}
+
+const makeSprites = function() {
+    const gotuParent = document.querySelector('.gotu');
+    const gotuCreate = document.createElement('div');
+    const frizuParent = document.querySelector('.frizu');
+    const frizuCreate = document.createElement('div');
+    gotuCreate.className = "gotu-standing";
+    frizuCreate.className = "frizu-standing";
+    gotuParent.appendChild(gotuCreate);
+    frizuParent.appendChild(frizuCreate);
+}
+
+const makeHealthBars = function() {
+    const healthBarParent = document.querySelector('.health-bar');
+    const gotuHealth = document.createElement('progress');
+    const frizuHealth = document.createElement('progress');
+    gotuHealth.id = "p1health";
+    gotuHealth.value = "100";
+    gotuHealth.max = "100";
+    frizuHealth.id = "p2health";
+    frizuHealth.value = "100";
+    frizuHealth.max = "100";
+    healthBarParent.appendChild(gotuHealth);
+    healthBarParent.appendChild(frizuHealth);
+}
+
+// const makePowerUps = function() {
+//     const powerUpParent = document.querySelector('.sprites');
+//     const gotuAura = document.createElement('div');
+//     const frizuAura = document.createElement('div');
+//     gotuAura.className = "gotu-aura";
+//     frizuAura.className = "frizu-aura";
+//     powerUpParent.appendChild(gotuAura);
+//     powerUpParent.appendChild(frizuAura);
+
+// }
+
+
+// <progress id="p1health" value="100" max="100"></progress>
+// <progress id="p2health" value="100" max="100"></progress>
 
 
 document.onkeydown = function(e) {
@@ -15,22 +62,13 @@ document.onkeydown = function(e) {
   
     if (e.keyCode === p1Down) {
         e.preventDefault();
-        
-        
-        gotu.classList.add('gotu-down');
-        setInterval(()=> {gotu.classList.remove('gotu-down')}, 1000)
-        if (parseInt(gotu.style.top) <= 240) {
+        if (parseInt(gotu.style.top) <= 210) {
             gotuDown = parseInt(gotu.style.top);
             gotu.style.top = (gotuDown + 15) + 'px';
-            
-            
         }
     } else if (e.keyCode === p1Up) {
         e.preventDefault();
-        gotu.classList.toggle('gotu-up');
-        gotu.classList.remove('gotu-down');
-        // gotu.classList.remove('gotu-standing')
-        gotuUp = parseInt(gotu.style.top || 250);
+        gotuUp = parseInt(gotu.style.top || 180);
         gotu.style.top = (gotuUp - 15) + 'px';
           if (parseInt(gotu.style.top) <= 10) {
             gotu.style.top = (gotuUp) + 'px';
@@ -40,12 +78,11 @@ document.onkeydown = function(e) {
         const kamehameha = document.createElement('div')
         kamehameha.className = "kamehameha"
         document.body.appendChild(kamehameha);
-        kamehamehaGPST = parseInt(gotu.style.top || 250);
+        kamehamehaGPST = parseInt(gotu.style.top || 180);
         kamehameha.style.top = ((kamehamehaGPST*1.5)+22) + 'px';
         kamehameha.style.left = 128 + 'px';
         setTimeout(function() {
             const gotu = document.querySelector('.gotu-standing');
-            //emre mentinoed getBoundingClientRect()
             const gotuDiv = gotu.getBoundingClientRect();
             const kamehameha = document.querySelector('.kamehameha');
             const kamehamehaDiv = kamehameha.getBoundingClientRect();
@@ -54,7 +91,6 @@ document.onkeydown = function(e) {
             const kameAdjTop = ((kamehamehaDiv.top-22))/1.5+24;
             const kameAdjBot = kameAdjTop + 12;
             if (kameAdjBot > frizuDiv.top && kameAdjTop < frizuDiv.top) {
-                //https://stackoverflow.com/questions/20277052/how-to-make-a-health-bar
                 let healthPoints2 = document.querySelector("#p2health")
                 healthPoints2.value -= 5;
                 checkHealthP2();
@@ -71,13 +107,13 @@ document.onkeydown = function(e) {
         }, 501);
     }  else if (e.keyCode === p2Down) {
         e.preventDefault();
-        if (parseInt(frizu.style.top) <= 240) {
+        if (parseInt(frizu.style.top) <= 210) {
             frizuDown = parseInt(frizu.style.top);
             frizu.style.top = (frizuDown + 15) + 'px';
         }
     } else if (e.keyCode === p2Up) {
         e.preventDefault();
-        frizuUp = parseInt(frizu.style.top || 250);
+        frizuUp = parseInt(frizu.style.top || 180);
         frizu.style.top = (frizuUp - 15) + 'px';
           if (parseInt(frizu.style.top) <= 10) {
             frizu.style.top = (frizuUp) + 'px';
@@ -86,12 +122,11 @@ document.onkeydown = function(e) {
         const kamehameha2 = document.createElement('div')
         kamehameha2.className = "kamehameha2"
         document.body.appendChild(kamehameha2);
-        kamehamehaGPST2 = parseInt(frizu.style.top || 250);
+        kamehamehaGPST2 = parseInt(frizu.style.top || 180);
         kamehameha2.style.top = ((kamehamehaGPST2*1.5)+22) + 'px';
         kamehameha2.style.left = 128 + 'px';
         setTimeout(function() {
             const frizu = document.querySelector('.frizu-standing');
-            //emre mentinoed getBoundingClientRect()
             const frizuDiv = frizu.getBoundingClientRect();
             const kamehameha2 = document.querySelector('.kamehameha2');
             const kamehameha2Div = kamehameha2.getBoundingClientRect();
@@ -100,7 +135,6 @@ document.onkeydown = function(e) {
             const kameAdjTop2 = ((kamehameha2Div.top)-22)/1.5+24;
             const kameAdjBot2 = kameAdjTop2+12;
             if (kameAdjBot2 > gotuDiv.top && kameAdjTop2 < gotuDiv.top) {
-                //https://stackoverflow.com/questions/20277052/how-to-make-a-health-bar
                 let healthPoints = document.querySelector("#p1health")
                     healthPoints.value -= 5;
                     checkHealthP1();
